edb622774720f964fdc5529d231805266eefdda3
acropr <acropr@acropr-VirtualBox>
2021-09-09 18:42:18 -0400

    Test

---
+++ myfunc.h
@@ -0,0 +1,15 @@
+/*
+ * myfunc.h
+ *
+ *  Created on: Sep 9, 2021
+ *      Author: acropr
+ */
+
+#ifndef MYFUNC_H_
+#define MYFUNC_H_
+#include <string>
+
+std::string myfunc();
+
+
+#endif /* MYFUNC_H_ */
